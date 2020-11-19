c461e42e420155b63681f5679ba21fd5b72176cb
vvv <vvv@ss.com>
2020-11-19 21:00:05 +0800

    add debug info

--- src/test.py
+++ src/test.py
@@ -67,9 +67,46 @@ def prefetch_test(opt):
   time_stats = ['tot', 'load', 'pre', 'net', 'dec', 'post', 'merge']
   avg_time_stats = {t: AverageMeter() for t in time_stats}
   for ind, (img_id, pre_processed_images) in enumerate(data_loader):
-    import pdb
-    pdb.set_trace()
+    # ind 0
+    # img_id tensor([397133])
+    
+    ## test
+    # pre_processed_images.keys() dict_keys(['images', 'image', 'meta'])
+    # pre_processed_images['images'].keys() dict_keys([1.0])
+    # pre_processed_images['images'][1.0].shape torch.Size([1, 1, 3, 512, 768])
+    # pre_processed_images['image'].shape torch.Size([1, 427, 640, 3]) always true image size
+    # pre_processed_images['meta'] 
+    # {1.0: {'c': tensor([[320., 213.]]), 's': tensor([[768., 512.]]), 'out_height': tensor([128]), 'out_width': tensor([192])}}
+
+    ## test_flip
+    # pre_processed_images.keys() dict_keys(['images', 'image', 'meta'])
+    # pre_processed_images['images'].keys() dict_keys([1.0])
+    # pre_processed_images['images'][1.0].shape torch.Size([1, 2, 3, 512, 768])
+    # pre_processed_images['image'].shape torch.Size([1, 427, 640, 3]) always true image size
+    # pre_processed_images['meta'] 
+    # {1.0: {'c': tensor([[320., 213.]]), 's': tensor([[768., 512.]]), 'out_height': tensor([128]), 'out_width': tensor([192])}}    
+
+    ## test_scale
+    # pre_processed_images.keys() dict_keys(['images', 'image', 'meta'])
+    # pre_processed_images['images'].keys() dict_keys([0.5, 0.75, 1.0, 1.25, 1.5])
+    # pre_processed_images['images'][1.0].shape torch.Size([1, 2, 3, 512, 768])
+    # pre_processed_images['image'].shape torch.Size([1, 427, 640, 3]) always true image size
+    # pre_processed_images['meta'] 
+    # {0.5: {'c': tensor([[160., 106.]]), 's': tensor([[384., 256.]]), 'out_height': tensor([64]), 'out_width': tensor([96])}, 0.75: {'c': tensor([[240., 160.]]), 's': tensor([[512., 384.]]), 'out_height': tensor([96]), 'out_width': tensor([128])}, 1.0: {'c': tensor([[320., 213.]]), 's': tensor([[768., 512.]]), 'out_height': tensor([128]), 'out_width': tensor([192])}, 1.25: {'c': tensor([[400., 266.]]), 's': tensor([[896., 640.]]), 'out_height': tensor([160]), 'out_width': tensor([224])}, 1.5: {'c': tensor([[480., 320.]]), 's': tensor([[1024.,  768.]]), 'out_height': tensor([192]), 'out_width': tensor([256])}}
+
     ret = detector.run(pre_processed_images)
+    ## test_scale
+    # ret.keys()
+    # dict_keys(['results', 'tot', 'load', 'pre', 'net', 'dec', 'post', 'merge'])
+    # ret['tot'] 0.4539330005645752
+    # ret['dec'] 0.004953622817993164
+    # ...pre always time
+    # ret['results'].keys()
+    # always whatever flip scale or none dict_keys([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80])
+    # ret['results'][1].shape (0, 5)
+    # ret['results'][2].shape (0, 5)
+    # ret['results'][70].shape (4, 5)
+
     results[img_id.numpy().astype(np.int32)[0]] = ret['results']
     Bar.suffix = '[{0}/{1}]|Tot: {total:} |ETA: {eta:} '.format(
                    ind, num_iters, total=bar.elapsed_td, eta=bar.eta_td)
