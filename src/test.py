5675df226c17d44dc8d066d303a3f65c07bbd489
vvv <vvv@ss.com>
2020-11-19 20:30:19 +0800

    test

--- src/test.py
+++ src/test.py
@@ -56,12 +56,10 @@ def prefetch_test(opt):
   split = 'val' if not opt.trainval else 'test'
   dataset = Dataset(opt, split)
   detector = Detector(opt)
-  
-  print(1)
 
   data_loader = torch.utils.data.DataLoader(
     PrefetchDataset(opt, dataset, detector.pre_process), 
-    batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+    batch_size=1, shuffle=False, num_workers=0, pin_memory=True)
 
   results = {}
   num_iters = len(dataset)
@@ -69,6 +67,8 @@ def prefetch_test(opt):
   time_stats = ['tot', 'load', 'pre', 'net', 'dec', 'post', 'merge']
   avg_time_stats = {t: AverageMeter() for t in time_stats}
   for ind, (img_id, pre_processed_images) in enumerate(data_loader):
+    import pdb
+    pdb.set_trace()
     ret = detector.run(pre_processed_images)
     results[img_id.numpy().astype(np.int32)[0]] = ret['results']
     Bar.suffix = '[{0}/{1}]|Tot: {total:} |ETA: {eta:} '.format(
