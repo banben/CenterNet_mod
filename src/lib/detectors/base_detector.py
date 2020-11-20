664e920d4648f695c838d794c171976882717a23
vvv <vvv@ss.com>
2020-11-20 15:29:53 +0800

    remove test code

--- src/lib/detectors/base_detector.py
+++ src/lib/detectors/base_detector.py
@@ -62,18 +62,11 @@ class BaseDetector(object):
 
     trans_input = get_affine_transform(c, s, 0, [inp_width, inp_height])
     resized_image = cv2.resize(image, (new_width, new_height))
-    import pickle
-    with open('/tmp/resized_image.pkl', 'wb') as f:
-      pickle.dump(resized_image, f)
     # resized_image.shape (213, 320, 3)
     inp_image = cv2.warpAffine(
       resized_image, trans_input, (inp_width, inp_height),
       flags=cv2.INTER_LINEAR)
     # inp_image.shape (256, 384, 3)
-    with open('/tmp/inp_image.pkl', 'wb') as f:
-      pickle.dump(inp_image, f)
-    import pdb
-    pdb.set_trace()
     inp_image = ((inp_image / 255. - self.mean) / self.std).astype(np.float32)
 
     images = inp_image.transpose(2, 0, 1).reshape(1, 3, inp_height, inp_width)
